e59b8d0a03a57f7268f3ba50f3ec5b10c98b3766
sigmapie8 <garg.manav8@gmail.com>
2023-04-24 11:39:58 +0530

    refactor: Removed unused imports

--- bikeshare_2.py
+++ bikeshare_2.py
@@ -1,8 +1,6 @@
 import time
 import pandas as pd
-import numpy as np
 from enum import Enum
-import datetime
 
 CITY_DATA = { 'chicago': 'chicago.csv',
               'new york city': 'new_york_city.csv',
